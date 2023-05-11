8f371ea226585d14fcbaf3a7e1776a0f86944115
JellyKingdom <jyhan1993@gmail.com>
2023-05-11 18:57:12 +0900

    Feat: image optimization

--- app/list/page.js
+++ app/list/page.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import food from '/public/food0.png'
+
 export default function List() {
   let products = ["Tomatoes", "Pasta", "Coconut"];
   let array = [2, 3, 4];
@@ -8,7 +11,8 @@ export default function List() {
 
       {products.map((item, index) => {
         return (
-          <div className='food'>
+          <div className='food' key={index}>
+            <Image src={food} className="food-img"/>
             <p>{index}</p>
             <h2>{item}</h2>
             <h4>$40</h4>
