344b3aebac757bc5f66a250308975f35d1dd2276
JellyKingdom <jyhan1993@gmail.com>
2023-05-11 17:08:51 +0900

    Feat: routes and layout

---
+++ app/cart/payment/page.js
@@ -0,0 +1,7 @@
+export default function Payment() {
+  return (
+    <div>
+      <h2 className='title'>결제페이지 입니다.</h2>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,15 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  let name = "han";
-  let link = "http://google.com";
   return (
-    <div>
-      <h4 className='title'>애플후레시</h4>
-      <p className='title-sub'>by {name}</p>
-      <p style={{fontSize:'30px'}}>야호</p>
-      <a href={link}>링크</a>
-    </div>
+    <main>
+      <h1 className='title'>Apple Fresh</h1>
+      <p className='title-sub'>by nanana</p>
+    </main>
   );
 }
