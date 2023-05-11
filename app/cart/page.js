46f55955aa50282d82d19e18f4a120e5c1b48743
JellyKingdom <jyhan1993@gmail.com>
2023-05-11 19:37:35 +0900

    client/server component, import

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,43 @@
+// 'use client'
+
+import {age, name} from "./data";
+import Test from "./test";
+
+//파일 맨 위에 'use client' 넣고 만든건 clint component
+// server component
+// - html에 자바스크립트 기능넣기 불가능
+// - 예를들어 div안에 onClick같은거 넣으면 에러난다.
+// - useState, useEffect 사용불가
+// - 로딩속도 빠름
+// - 검색엔진 노출 유리
+// - 큰페이지
+
+// client component
+// - 로딩속도 느림(자바스크립트 많이 필요)
+// - hydration 필요 (html 유저에게 보낸 후에 자바스크립트로 html 다시 읽고 분석하는 일)
+// - js기능이 필요한 곳
+
 export default function Cart() {
   return (
     <div>
-      <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
+        <Test/>
+      <h4 className='title'>Cart</h4>
+      <div className='cart-item'>
+        <p>{name} {age}</p>
         <p>$40</p>
         <p>1개</p>
       </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem/>
     </div>
-  )
-} 
+  );
+}
+
+function CartItem(props) {
+  return (
+    <div className='cart-item'>
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
+}
---
+++ app/cart/test.js
@@ -0,0 +1,6 @@
+export default function Test() {
+    return (
+      <h1>안녕하세요</h1>
+    );
+  }
+  
