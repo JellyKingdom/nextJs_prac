6c421a9fafc7ac408161e4598cc754f2d4d9eef7
JellyKingdom <jyhan1993@gmail.com>
2023-05-11 16:49:18 +0900

    feat: react-basic

--- app/page.js
+++ app/page.js
@@ -2,7 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (<div>
-    
-  </div>);
+  let name = "han";
+  let link = "http://google.com";
+  return (
+    <div>
+      <h4 className='title'>애플후레시</h4>
+      <p className='title-sub'>by {name}</p>
+      <p style={{fontSize:'30px'}}>야호</p>
+      <a href={link}>링크</a>
+    </div>
+  );
 }
